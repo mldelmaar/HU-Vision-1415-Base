5306f76c9628cd0420a14f3bb3b50927de325b6f
Biancakr <bianca.krieger@student.hu.nl>
2015-08-21 22:06:48 +0200

    doxygen studentLocalization

--- source/ExternalDLL/ExternalDLL/StudentLocalization.h
+++ source/ExternalDLL/ExternalDLL/StudentLocalization.h
@@ -4,6 +4,9 @@
 * Proprietary and confidential
 */
 
+/**
+* @Author Bianca Krieger 
+*/
 #pragma once
 #include "Localization.h"
 #include "IntensityImageStudent.h"
@@ -19,15 +22,31 @@ public:
 	bool stepFindChinContours(const IntensityImage &image, FeatureMap &features) const;
 	bool stepFindNoseEndsAndEyes(const IntensityImage &image, FeatureMap &features) const;
 	bool stepFindExactEyes(const IntensityImage &image, FeatureMap &features) const;
-
+	
+	/** @brief counts for a certain vertical range, from y to y + alpha, the amount 
+	*		   of pixels on every x location that have a low intensity (black)
+	*   @param &image a reference to the intensity image 
+	*   @param alpha the height range
+	*   @param startY the top if the y in the range
+	*	@return histogram a array of the amount the low intensity pixels for 
+	*			every x position within the given y range
+	*/
 	static int * getHistogramX(const IntensityImage &image, int alpha, int startY);
 
+	/** @brief detects the left and right sides in the histogram which are above 
+	*		   certain thresholds.
+	*   @param *histogram to find the sides in
+	*   @param size of the histgram and also the width of the image
+	*   @param *first side that is found in the histogram
+	*	@param *last side that is found in the histogram
+	*/
 	static void findSidesInHistogram(int* histogram, int size, int* first, int * last);
 
+	/** @brief detects the first low intensity pixel starting from the upper left corner
+	*          of the image which indicates the top of the head  
+	*   @param &image a reference to the intensity image
+	*	@return the y of the first low intensity pixel		
+	*/
 	static int getTopOfHead(const IntensityImage &image);
-
-	//Test
-	//static int * getIntensityHistogram(const IntensityImage &image);
-	//static IntensityImage * getIntensityHistogramImage(const IntensityImage &image);
 };
 
